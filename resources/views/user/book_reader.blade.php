67f04e411af7fed810c30804d24f62f002173ed0
Sodik Owolabi <siresodmond@gmail.com>
2024-10-02 10:09:29 +0100

    Update book_reader.blade.php

--- resources/views/user/book_reader.blade.php
+++ resources/views/user/book_reader.blade.php
@@ -5,10 +5,11 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                {{--@if(session('book_file_path'))--}}
+                <div class="text-center mb-3">
+                    <h2>{{ $book->title }}</h2>
+                </div>
                 <object class="pdf" data="{{ asset('storage/books/files/'. $book->book_file) .'#toolbar=0' }}"
                     width="800" height="500"></object>
-                {{--@endif--}}
                 <div id="book_reader"></div>
             </div>
         </div>
