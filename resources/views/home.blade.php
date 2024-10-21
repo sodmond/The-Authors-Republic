8cdc0a5202b9c68fd9b2aca757d5d717fb255fed
Sodik Owolabi <siresodmond@gmail.com>
2024-10-21 11:04:42 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -149,66 +149,72 @@
 		Featured Item End
 *************************************-->
 <!--************************************
-        Latest News Start
+        New Release Start
 *************************************-->
 <section class="tg-sectionspace tg-haslayout">
     <div class="container">
         <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                <div class="tg-sectionhead">
-                    <h2><span>Latest News &amp; Articles</span>What's Hot in The News</h2>
-                    <a class="tg-btn" href="{{ route('news') }}">View All</a>
-                </div>
-                <div class="row">
-                    @foreach ($latestNews as $article)
-                    <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
-                        @php 
-                            $slug = \App\Models\Article::getSlug($article->title);
-                            $link = route('news.get', ['id' => $article->id, 'slug' => $slug])
-                        @endphp
-                        <article class="tg-post">
-                            <figure><a href="{{ $link }}"><img src="{{ asset('storage/'.$article->image) }}" alt="Articles Image"></a></figure>
-                            <div class="tg-postcontent">
-                                <ul class="tg-postmetadata">
-                                    <li><i class="fa fa-calendar"></i> Published on: {{ date('M j, Y', strtotime($article->published_at)) }}</li>
-                                </ul>
-                                <ul class="tg-bookscategories"></ul>
-                                <div class="tg-posttitle">
-                                    <h3><a href="{{ $link }}">{{ strtoupper($article->title) }}</a></h3>
-                                </div>
-                                <span class="tg-bookwriter">By: <a href="javascript:void(0);">Admin</a></span>
-                            </div>
-                        </article>
+            <div class="tg-newrelease">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <div class="tg-sectionhead">
+                        <h2><span>Taste The New Spice</span>New Release Books</h2>
                     </div>
-                    @endforeach
+                    <div class="tg-products">
+                        <div class="tg-productgrid">
+                            <div class="row">
+                                @foreach($books as $book)
+                                <div class="col-xs-6 col-sm-6 col-md-4 col-lg-2 col-6">
+                                    <div class="tg-postbook">
+                                        <figure class="tg-featureimg">
+                                            <div class="tg-bookimg">
+                                                @php $bookImage = ($book->image != '') ? asset('storage/'.$book->image) : asset('frontend/images/books/img-01.jpg') @endphp
+                                                <div class="tg-frontcover"><img src="{{ $bookImage }}" alt="image description"></div>
+                                                <div class="tg-backcover"><img src="{{ $bookImage }}" alt="image description"></div>
+                                            </div>
+                                            <form action="{{ route('user.wishlist.add') }}" method="post">
+                                                @csrf
+                                                <input type="hidden" name="book_id" value="{{ $book->id }}">
+                                                <button type="submit" class="tg-btnaddtowishlist">
+                                                    <i class="icon-heart"></i>
+                                                    <span>add to wishlist</span>
+                                                </button>
+                                            </form>
+                                        </figure>
+                                        <div class="tg-postbookcontent">
+                                            <ul class="tg-bookscategories">
+                                                <li><a href="javascript:void(0);">{{ \App\Models\Book::getCategoryName($book->category_id) }}</a></li>
+                                            </ul>
+                                            <div class="tg-booktitle">
+                                                @php $slug = \App\Models\Book::getSlug($book->title); @endphp
+                                                <h3><a href="{{ route('book', ['id' => $book->id, 'slug' => $slug]) }}">{{ ucwords($book->title) }}</a></h3>
+                                            </div>
+                                            <span class="tg-bookwriter">By: <a href="javascript:void(0);">{{ $book->author->firstname.' '.$book->author->lastname }}</a></span>
+                                            <span class="tg-bookprice">
+                                                <ins>₦{{ number_format($book->price, 2) }}</ins>
+                                            </span>
+                                            <form method="POST" action="{{ route('cart.add') }}">
+                                                @csrf
+                                                <input type="hidden" name="book_id" value="{{ $book->id }}">
+                                                <input type="hidden" name="quantity" value="1">
+                                                <button class="tg-btn tg-btnstyletwo" href="javascript:void(0);">
+                                                    <i class="fa fa-shopping-cart"></i>
+                                                    <em>Add To Cart</em>
+                                                </button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            {{--<div id="tg-postslider" class="tg-postslider tg-blogpost owl-carousel">
-                @foreach($latestNews as $news)
-                <article class="item tg-post">
-                    @php 
-                        $slug = \App\Models\Article::getSlug($news->title);
-                        $link = route('news.get', ['id' => $news->id, 'slug' => $slug]);
-                        $newsImage = ($news->image != '') ? asset('storage/'.$news->image) : asset('frontend/images/blog/img-01.jpg');
-                    @endphp
-                    <figure><a href="{{ $link }}"><img src="{{ $newsImage }}" alt="article image"></a></figure>
-                    <div class="tg-postcontent">
-                        <div class="tg-posttitle">
-                            <h3><a href="{{ $link }}">{{ $news->title }}</a></h3>
-                        </div>
-                        <span class="tg-bookwriter">By: <a href="javascript:void(0);">Admin</a></span>
-                        <ul class="tg-postmetadata">
-                            <li><a href="javascript:void(0);"><i class="fa fa-calendar"></i><i>Published on: {{ date('M j, Y', strtotime($news->published_at)) }}</i></a></li>
-                        </ul>
-                    </div>
-                </article>
-                @endforeach
-            </div>--}}
         </div>
     </div>
 </section>
 <!--************************************
-		Latest News End
+        New Release End
 *************************************-->
 <!--************************************
         Call to Action Start
@@ -333,72 +339,66 @@
         Testimonials End
 *************************************-->
 <!--************************************
-        New Release Start
+        Latest News Start
 *************************************-->
 <section class="tg-sectionspace tg-haslayout">
     <div class="container">
         <div class="row">
-            <div class="tg-newrelease">
-                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                    <div class="tg-sectionhead">
-                        <h2><span>Taste The New Spice</span>New Release Books</h2>
-                    </div>
-                    <div class="tg-products">
-                        <div class="tg-productgrid">
-                            <div class="row">
-                                @foreach($books as $book)
-                                <div class="col-xs-6 col-sm-6 col-md-4 col-lg-2 col-6">
-                                    <div class="tg-postbook">
-                                        <figure class="tg-featureimg">
-                                            <div class="tg-bookimg">
-                                                @php $bookImage = ($book->image != '') ? asset('storage/'.$book->image) : asset('frontend/images/books/img-01.jpg') @endphp
-                                                <div class="tg-frontcover"><img src="{{ $bookImage }}" alt="image description"></div>
-                                                <div class="tg-backcover"><img src="{{ $bookImage }}" alt="image description"></div>
-                                            </div>
-                                            <form action="{{ route('user.wishlist.add') }}" method="post">
-                                                @csrf
-                                                <input type="hidden" name="book_id" value="{{ $book->id }}">
-                                                <button type="submit" class="tg-btnaddtowishlist">
-                                                    <i class="icon-heart"></i>
-                                                    <span>add to wishlist</span>
-                                                </button>
-                                            </form>
-                                        </figure>
-                                        <div class="tg-postbookcontent">
-                                            <ul class="tg-bookscategories">
-                                                <li><a href="javascript:void(0);">{{ \App\Models\Book::getCategoryName($book->category_id) }}</a></li>
-                                            </ul>
-                                            <div class="tg-booktitle">
-                                                @php $slug = \App\Models\Book::getSlug($book->title); @endphp
-                                                <h3><a href="{{ route('book', ['id' => $book->id, 'slug' => $slug]) }}">{{ ucwords($book->title) }}</a></h3>
-                                            </div>
-                                            <span class="tg-bookwriter">By: <a href="javascript:void(0);">{{ $book->author->firstname.' '.$book->author->lastname }}</a></span>
-                                            <span class="tg-bookprice">
-                                                <ins>₦{{ number_format($book->price, 2) }}</ins>
-                                            </span>
-                                            <form method="POST" action="{{ route('cart.add') }}">
-                                                @csrf
-                                                <input type="hidden" name="book_id" value="{{ $book->id }}">
-                                                <input type="hidden" name="quantity" value="1">
-                                                <button class="tg-btn tg-btnstyletwo" href="javascript:void(0);">
-                                                    <i class="fa fa-shopping-cart"></i>
-                                                    <em>Add To Cart</em>
-                                                </button>
-                                            </form>
-                                        </div>
-                                    </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <div class="tg-sectionhead">
+                    <h2><span>Latest News &amp; Articles</span>What's Hot in The News</h2>
+                    <a class="tg-btn" href="{{ route('news') }}">View All</a>
+                </div>
+                <div class="row">
+                    @foreach ($latestNews as $article)
+                    <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
+                        @php 
+                            $slug = \App\Models\Article::getSlug($article->title);
+                            $link = route('news.get', ['id' => $article->id, 'slug' => $slug])
+                        @endphp
+                        <article class="tg-post">
+                            <figure><a href="{{ $link }}"><img src="{{ asset('storage/'.$article->image) }}" alt="Articles Image"></a></figure>
+                            <div class="tg-postcontent">
+                                <ul class="tg-postmetadata">
+                                    <li><i class="fa fa-calendar"></i> Published on: {{ date('M j, Y', strtotime($article->published_at)) }}</li>
+                                </ul>
+                                <ul class="tg-bookscategories"></ul>
+                                <div class="tg-posttitle">
+                                    <h3><a href="{{ $link }}">{{ strtoupper($article->title) }}</a></h3>
                                 </div>
-                                @endforeach
+                                <span class="tg-bookwriter">By: <a href="javascript:void(0);">Admin</a></span>
                             </div>
-                        </div>
+                        </article>
                     </div>
+                    @endforeach
                 </div>
             </div>
+            {{--<div id="tg-postslider" class="tg-postslider tg-blogpost owl-carousel">
+                @foreach($latestNews as $news)
+                <article class="item tg-post">
+                    @php 
+                        $slug = \App\Models\Article::getSlug($news->title);
+                        $link = route('news.get', ['id' => $news->id, 'slug' => $slug]);
+                        $newsImage = ($news->image != '') ? asset('storage/'.$news->image) : asset('frontend/images/blog/img-01.jpg');
+                    @endphp
+                    <figure><a href="{{ $link }}"><img src="{{ $newsImage }}" alt="article image"></a></figure>
+                    <div class="tg-postcontent">
+                        <div class="tg-posttitle">
+                            <h3><a href="{{ $link }}">{{ $news->title }}</a></h3>
+                        </div>
+                        <span class="tg-bookwriter">By: <a href="javascript:void(0);">Admin</a></span>
+                        <ul class="tg-postmetadata">
+                            <li><a href="javascript:void(0);"><i class="fa fa-calendar"></i><i>Published on: {{ date('M j, Y', strtotime($news->published_at)) }}</i></a></li>
+                        </ul>
+                    </div>
+                </article>
+                @endforeach
+            </div>--}}
         </div>
     </div>
 </section>
 <!--************************************
-        New Release End
+		Latest News End
 *************************************-->
 @endsection
 
